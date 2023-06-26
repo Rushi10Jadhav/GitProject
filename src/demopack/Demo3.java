f2896a26e7774bb699acf97185792deb7615e706
Rushi Jadhav <srushikeshjadhav.10@gmail.com>
2023-06-26 17:45:51 +0530

    New classes added

--- src/demopack/Demo3.java
+++ src/demopack/Demo3.java
@@ -1,9 +1,8 @@
 package demopack;
 
-public class Demo2 {
+public class Demo3 {
 
 	public static void main(String[] args) {
-
 		System.out.println("This is the main method");
 		System.out.println("This is simple class");
 	}
